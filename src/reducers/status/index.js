06d660768ffa15e4435c4cf76f3b7ce8133f46d7
Benjamin Beret <benjamin.beret@gmail.com>
2016-11-26 16:58:48 +0100

    Rename flightPlanRequest to profiles in status object for consistency with the rest of the codebase

--- src/reducers/status/index.js
+++ src/reducers/status/index.js
@@ -3,10 +3,10 @@ import { combineReducers } from 'redux';
 
 import autocomplete from './autocomplete';
 import flightKeys from './flightKeys';
-import flightPlanRequest from './flight-plan-request';
+import profiles from './profiles';
 
 export default combineReducers({
   autocomplete,
   flightKeys,
-  flightPlanRequest,
+  profiles,
 });
